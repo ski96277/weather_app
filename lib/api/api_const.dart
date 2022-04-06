bf6d291fe7bb19dc15423ec75d2e170dcad723b8
hossain.iqminds <Dev2021#!>
2022-04-06 10:40:50 +0600

    done

--- lib/api/api_const.dart
+++ lib/api/api_const.dart
@@ -4,5 +4,5 @@ class ApiConst {
   static String baseUrl = "https://api.openweathermap.org/";
 
 // static String weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=23.8103&lon=90.4125&appid=ffc95e0d98b87e8eb03351fd3a200213"
-  static String weatherUrl = baseUrl + "data/2.5/weather?lat=#lat#&lon=#lon#&appid=${StaticKeyWord.weatherAppID}&units=Metric";
+  static String weatherUrl = "data/2.5/weather?lat=#lat#&lon=#lon#&appid=${StaticKeyWord.weatherAppID}&units=Metric";
 }
